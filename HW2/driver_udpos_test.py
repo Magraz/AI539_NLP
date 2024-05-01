64f5865bed18ed174ad20ebb92b796f947037632
Magraz <agrazvallejo@live.com>
2024-04-30 19:42:42 -0700

    added except

--- HW2/driver_udpos_test.py
+++ HW2/driver_udpos_test.py
@@ -147,11 +147,16 @@ def train_model(model, train_loader, x_map, y_map, epochs=2000, lr=1e-2):
                     }, 'model.pth')
             
             #Store loss
-            pickle_train_loss = []
-            with open('train_loss.pickle', 'rb') as handle:
-                pickle_train_loss = pickle.load(handle)
-                pickle_train_loss.append(train_loss)
-                logging.info(f'Total Epochs: {len(pickle_train_loss)}')
+            try:
+                pickle_train_loss = []
+                with open('train_loss.pickle', 'rb') as handle:
+                    pickle_train_loss = pickle.load(handle)
+                    pickle_train_loss.append(train_loss)
+                    logging.info(f'Total Epochs: {len(pickle_train_loss)}')
+            except FileNotFoundError:
+                logging.info('Writing new loss file...')
+                with open('train_loss.pickle', 'wb') as handle:
+                    pickle.dump(train_loss, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
             with open('train_loss.pickle', 'wb') as handle:
                 pickle.dump(pickle_train_loss, handle, protocol=pickle.HIGHEST_PROTOCOL)
